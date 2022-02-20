785972707bdbe6e4090e1e416b35f5f9aa63764b
EyitopeIO <adeyitope.io@gmail.com>
2022-02-20 03:19:37 +0000

    Include kernel headers to be used.

--- driver-recipe/hcsr04.c
+++ driver-recipe/hcsr04.c
@@ -8,3 +8,14 @@
 * Driver for HC-SR04 ultrasonic sensor usigng sys file system (SYS) and virtual file system (VFS)
 *
 */
+
+
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+
+static dev_t major_number;
+
+// MAJOR(dev_t dev);
+// MINOR(dev_t dev);
+// MKDEV(major_num, minor_num);
