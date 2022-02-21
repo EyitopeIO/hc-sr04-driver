b189078c976f47afa964f1338fa669ed832094d0
EyitopeIO <adeyitope.io@gmail.com>
2022-02-21 20:31:08 +0000

    Define init function the more.

--- driver-recipe/hcsr04.c
+++ driver-recipe/hcsr04.c
@@ -11,11 +11,34 @@
 
 
 #include <linux/types.h>
+#include <linux/cdev.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/types.h>.
 #include <linux/kdev_t.h>
 #include <linux/fs.h>
 
-static dev_t major_number;
+static dev_t majorN;
+static cdev minorN;
 
 // MAJOR(dev_t dev);
 // MINOR(dev_t dev);
 // MKDEV(major_num, minor_num);
+
+struct file_operations hcsr04_ops = {
+    .owner = THIS_MODULE;
+    .read = hcsr04_read;
+    .write = hcsr04_write;
+}
+
+
+static int __init hcsro4_init(void) 
+{
+    alloc_chrdev_region(&majorN, 0, 1, "hcsr04");
+    cdev_init(&minorN, &hcsr04_ops);
+    hello_cdev.owner = THIS_MODULE;
+    cdev_add(&minorN, )
+    return 0;
+}
+
+
