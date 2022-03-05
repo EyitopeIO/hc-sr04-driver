50602cd46762d29470d28423d1e94d20c1051907
EyitopeIO <adeyitope.io@gmail.com>
2022-03-05 11:33:04 +0000

    Add verbose. Cound null byte when using write().

--- application-recipe/getdistance.c
+++ application-recipe/getdistance.c
@@ -53,12 +53,19 @@ void clearscreen(void)
 int main(int argc, char *argv[])
 {
 
+    if (argc < 2) {
+        printf("You must specify number of pings. Quitting.\n");
+        printf("Usage\t:getdistance <number_of_pings>\n");
+
+        return -1;
+    }
+
     int iter, cc, fd;     // Get from *argv[] and convert to int
     unsigned long duration = 0;
     double distance = 0.0;
     time_t ltime;
 
-    cc = atoi(*argv[1]);
+    cc = atoi(argv[1]);
     iter = cc;
 
     printf(" *** Test application for HC-SR04 driver ***\n");
@@ -67,27 +74,27 @@ int main(int argc, char *argv[])
     sleep(1);
 
     if ((fd = open(devname, O_RDWR)) < 0) {
-        perror(NULL);
+        perror("Exiting...");
         exit(EXIT_FAILURE);
     }
+    
+    printf("/dev/hcsr04 opened\n");
 
     while(iter--)
     {
-        write(fd, "1", 1);
+        write(fd, "1", 2);
         if ((bread = read(fd, &rcvd, sizeof(struct hcsr04_data)) < 0))
         {
             perror("read: FAIL");
             printf("Bytes read: %d\n", bread);
             continue;
         }
-
-        printf(
-            "\r-------------------------------------\n"
-            "                                       \n"
-            "Ping:\t\t%i of %i                      \n"
-            "Pulse duration:\t\t%d us               \n"
-            "Distance:\t\t%.1f cm                   \n",
-        (cc - iter), cc, rcvd.t_high, (double)((rcvd.t_high/1000)/DIVISION_CONSTANT));
+        printf("\r-------------------------------------\n"
+                "                                       \n"
+                "Ping:\t\t%i of %i                      \n"
+                "Pulse duration:\t\t%d us               \n"
+                "Distance:\t\t%.1f cm                   \n",
+        (cc - iter), cc, (int)(rcvd.t_high/1000), (double)((rcvd.t_high/1000)/DIVISION_CONSTANT));
         sleep(1);
         clearscreen();
     }
